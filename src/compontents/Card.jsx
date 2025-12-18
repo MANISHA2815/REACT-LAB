02926c92d3d89d702fdc707d014617ab596c6867
JoshikaThirumoorthi <24bcs027@kgcas.com>
2025-12-18 10:46:41 +0530

    first commit

---
+++ src/compontents/Card.jsx
@@ -0,0 +1,9 @@
+  export default function Card({fulname,occu}){
+    return(
+        <>
+        <h1>{fulname}</h1>
+        <h2>{occu}</h2>
+        </>
+    )
+}
+
---
+++ src/compontents/Dashboard.jsx
@@ -0,0 +1,13 @@
+
+
+function Dashboard({name, occu}) {
+    return(
+        
+        <>
+        <h1>{name}</h1>
+        <h2>{occu}</h2>
+        </>
+    )
+}
+
+export default Dashboard
